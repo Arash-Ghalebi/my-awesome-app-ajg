97f402fba45c638208d468cef5ca7beed5f3b87c
Arash Ghalebi <aghalebi33@gmail.com>
2022-01-31 16:10:40 -0500

    Test

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ const app = express()
 // Simple Hello World Node App
 const APP_PORT = process.env.APP_PORT || 3000; app.get('/', (req, res) => {
 
-    res.send('Hello to Arash and the rest of the world out there!')
+    res.send('Hello to Arash and the rest of the world out there! This is a test.')
 
 }); app.listen(APP_PORT, () => {
     console.log('Running app at port:${APP_PORT}')
